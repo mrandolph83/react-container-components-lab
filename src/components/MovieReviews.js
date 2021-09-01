e2ef0e75ad791a3c25a27b0957879e9224a34a69
mrandolph83 <mrandolph83@gmail.com>
2021-09-01 13:58:56 -0400

    Assignment Complete

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,12 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = ({ reviews }) => {
+ return <div className='review-list'>
+    {reviews.map((review) => {
+    return <p className="review">{review.byline}</p>
+    })}
+ </div>
+}
+
+export default MovieReviews
